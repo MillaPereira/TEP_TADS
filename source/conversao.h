33cde9712ec4d0257b4ee15ba55010fb4b69b62d
Milla Pereira <72027697+MillaPereira@users.noreply.github.com>
2021-05-11 12:03:50 -0300

    Update conversao.h

--- source/conversao.h
+++ source/conversao.h
@@ -16,6 +16,6 @@ D_Complexo_pt converteRacionalParaDouble(R_Complexo_pt rComplexo);
 
 I_Complexo_pt converteRacionalParaLongInt(R_Complexo_pt rComplexo);
 
-R_Complexo_pt converteLongIntPataRacional(I_Complexo_pt iComplexo);
+R_Complexo_pt converteLongIntParaRacional(I_Complexo_pt iComplexo);
 
 #endif
